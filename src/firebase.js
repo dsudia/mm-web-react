074277ba2f81fedff0844dd712e01cf7d336f554
David Sudia <dsudia@gmail.com>
2017-01-08 12:25:08 -0700

    added firebase file to import from

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+import * as firebase from 'firebase'
+
+const fb = firebase.initializeApp({
+  apiKey:  'AIzaSyCx9qGnCCieTChB0laygI1YZnkf4BuMRv8',
+  authDomain: 'www.montessorimatch.com',
+  databaseURL: 'https://montessori-match.firebaseio.com/',
+  storageBucket: 'gs://montessori-match.appspot.com'
+})
