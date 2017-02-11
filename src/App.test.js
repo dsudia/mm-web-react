d57e24c81a0cdd2e52905175179ad1efe5b4141f
David Sudia <dsudia@gmail.com>
2017-02-11 15:06:01 -0700

    should only test unit tests now

--- src/App.test.js
+++ src/App.test.js
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 it("renders without crashing", () => {
-  ReactDOM.render(<App />);
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
 });
--- src/index.js
+++ src/index.js
@@ -10,6 +10,13 @@ import getMuiTheme from "material-ui/styles/getMuiTheme";
 import { Route, Router, browserHistory } from "react-router";
 import injectTapEventPlugin from "react-tap-event-plugin";
 
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET
+});
+
 injectTapEventPlugin();
 
 ReactDOM.render(
