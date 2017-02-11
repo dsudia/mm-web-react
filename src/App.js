e1dbe194447cce91cdba5c314e9730b6aa10a76a
David Sudia <dsudia@gmail.com>
2017-02-11 14:59:07 -0700

    fixed test

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import Hero from "./components/hero/Hero";
 import AboutSchool from "./components/about/AboutSchool";
 import AboutTeacher from "./components/about/AboutTeacher";
 import "./App.css";
+import * as firebase from "firebase";
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET
+});
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -9,14 +9,6 @@ import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import getMuiTheme from "material-ui/styles/getMuiTheme";
 import { Route, Router, browserHistory } from "react-router";
 import injectTapEventPlugin from "react-tap-event-plugin";
-import * as firebase from "firebase";
-
-firebase.initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET
-});
 
 injectTapEventPlugin();
 
