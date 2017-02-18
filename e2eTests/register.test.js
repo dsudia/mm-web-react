8427877536e0055247a7fcdc83b1ca4ffd296460
David Sudia <dsudia@gmail.com>
2017-02-18 14:38:59 -0700

    removed e2etests directory

--- e2eTests/register.test.js
+++
@@ -1,8 +0,0 @@
-fixture `Register`
-  .page `http://localhost:3000`
-
-test(`Register a user with valid info`, async t => {
-    await t
-        .click("#profile-menu")
-        .click("#sign-up-button")
-})
