28a26b0a1faddbee2debf1114d4e0686b811d56c
David Sudia <dsudia@gmail.com>
2017-02-19 10:16:24 -0700

    added update match profile function

--- src/state/currentUser.js
+++ src/state/currentUser.js
@@ -3,6 +3,11 @@ import { action, observable } from 'mobx'
 export const initialState = observable({
   id: null,
   profile: null,
+  matchingProfile: null,
+  updateMatchingProfile: action.bound(function _updateMatchingProfile(newProps) {
+    const newProfile = Object.assign({}, this.matchingProfile || {}, newProps)
+    this.matchingProfile = newProfile
+  }),
   setId: action.bound(function _setCurrentUserId(id) {
     this.id = id
   }),
