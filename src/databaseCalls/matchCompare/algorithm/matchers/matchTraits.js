595e6fb554a6d0d1b099be3ef221fc18ec64624e
David Sudia <dsudia@fanatics.com>
2017-02-27 15:14:45 -0700

    made a var a const

--- src/databaseCalls/matchCompare/algorithm/matchers/matchTraits.js
+++ src/databaseCalls/matchCompare/algorithm/matchers/matchTraits.js
@@ -2,7 +2,7 @@ import { someMatch, countNumOfMatches, findDecimal } from "../calculators";
 
 export function matchTraits(memOneTraits, memTwoTraits) {
   if (someMatch(memOneTraits, memTwoTraits) === true) {
-    var count = countNumOfMatches(memOneTraits, memTwoTraits);
+    const count = countNumOfMatches(memOneTraits, memTwoTraits);
     return findDecimal(count, memOneTraits.length);
   } else {
     return 0;
